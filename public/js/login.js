fc0110424c2b521b51d89577a85d4a4467ba054b
Austn Hughes <austen.hughes.91@gmail.com>
2021-04-14 23:38:16 -0600

    working authentication

--- public/js/login.js
+++ public/js/login.js
@@ -1,26 +1,31 @@
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
-  // adjust to what is in our code 
-  const email = document.querySelector("email").value.trim();
-  const password = document.querySelector("#password").value.trim();
+  // Collect values from the login form
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+  console.log(email);
+  console.log(password);
 
   if (email && password) {
-    const response = await fetch("/api/users/login", {
-      method: "POST",
+    // Send a POST request to the API endpoint
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
       body: JSON.stringify({ email, password }),
-      headers: { "Content-Type": "application/json" },
+      headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-      document.location.replace("/");
+      // If successful, redirect the browser to the profile page
+      document.location.replace('/');
     } else {
-      alert("Failed to log in");
+      alert(response.statusText);
     }
   }
 };
 
-// adjust to what is in our code 
 document
-  .querySelector(".submit")
-  .addEventListener("submit", loginFormHandler);
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
+
+
