8b7b491cd1d6afebb28cb71a51ca0a9cfd1f04b5
MK <mihaelak08@gmail.com>
2021-04-16 11:38:11 -0700

    navigational changes

--- public/js/login.js
+++ public/js/login.js
@@ -2,22 +2,22 @@ const loginFormHandler = async (event) => {
   event.preventDefault();
 
   // Collect values from the login form
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
   console.log(email);
   console.log(password);
 
   if (email && password) {
     // Send a POST request to the API endpoint
-    const response = await fetch('/api/users/login', {
-      method: 'POST',
+    const response = await fetch("/api/users/login", {
+      method: "POST",
       body: JSON.stringify({ email, password }),
-      headers: { 'Content-Type': 'application/json' },
+      headers: { "Content-Type": "application/json" },
     });
 
     if (response.ok) {
       // If successful, redirect the browser to the profile page
-      document.location.replace('/');
+      document.location.replace("/");
     } else {
       alert(response.statusText);
     }
@@ -25,7 +25,5 @@ const loginFormHandler = async (event) => {
 };
 
 document
-  .querySelector('.login-form')
-  .addEventListener('submit', loginFormHandler);
-
-
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
